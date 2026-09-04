0b1614a1fb4bebfff7e8c42715679d41da9bbd97
Mavour <mavourgaming@gmail.com>
2026-09-04 13:25:39 +0800

    chore: serve production on port 1507 instead of 3000

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     {
       name: "hood-tracker",
       script: "node_modules/next/dist/bin/next",
-      args: "start -p 3000",
+      args: "start -p 1507",
       instances: 1,
       exec_mode: "fork",
       env: {
